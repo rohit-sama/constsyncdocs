cfabab24842b8a92875c522f182e8e0361cc5022
rohit-sama <rohit9804singh@gmail.com>
2024-01-20 22:55:59 +0530

    title

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'ConstSync',
+  tagline: 'Dark Patterns Detection Extension Documentation.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
